9c6aafd4ebd179cf1c807cf12ea058667cb0626d
fiyk <fiyk@fiyk.com>
2019-01-28 01:21:10 +0300

    v1

--- app.js
+++ app.js
@@ -1,64 +1,114 @@
 (function () {
     'use strict';
 
-    angular.module('AssignmentApp', [])
-        .controller('AssignmentController', AssignmentController);
+    var shoppingList = [
+        {
+            name: "Milk",
+            quantity: "34"
+        },
+        {
+            name: "Donuts",
+            quantity: "65"
+        },
+        {
+            name: "Cookies",
+            quantity: "25"
+        },
+        {
+            name: "Chocolate",
+            quantity: "54"
+        },
+        {
+            name: "Drink",
+            quantity: "10"
+        }
+    ];
 
-    AssignmentController.$inject = ['$scope'];
-    function AssignmentController($scope) {
-        $scope.name = "";
-        $scope.result = "";
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-        $scope.displayResult = function () {
-            var result = calculateFood($scope.name);
-            $scope.result = result;
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+        var tobuy = this;
+
+        tobuy.itemName = "";
+        tobuy.itemQuantity = "";
+        tobuy.items = ShoppingListCheckOffService.getItemsToBuy();
+
+        tobuy.addItem = function () {
+            ShoppingListCheckOffService.addItemToBuy(tobuy.itemName, tobuy.itemQuantity);
         };
 
+        tobuy.removeItem = function (itemIdex) {
+            ShoppingListCheckOffService.removeItemToBuy(itemIndex);
+        };
 
-        function calculateFood(string) {
-            var totalStringValue = 0;
-            var foodResult = "";
-
-            var str = $scope.name;
-
-            if (str != "") {
-                var words = str.split(',');
-
-                for (var i = 0; i < words.length; i++) {
-                    totalStringValue += 1;
-                }
-            }
-            else
-                totalStringValue = 0;
-
-            if (totalStringValue < 4 && totalStringValue > 0) {
-                foodResult = "Enjoy!";
-            }
-            else if (totalStringValue > 3) {
-                foodResult = "Too much!";
-            }
-            else {
-                foodResult = "Please enter data first";
-            }
-
-            return foodResult;
+        tobuy.buyToBought = function (itemIndex) {
+            ShoppingListCheckOffService.addItemAlreadyBought(tobuy.items[itemIndex].name, tobuy.items[itemIndex].quantity);
+            ShoppingListCheckOffService.removeItemToBuy(itemIndex);
         }
     }
 
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var alreadybought = this;
+
+        alreadybought.items = ShoppingListCheckOffService.getItemsAlreadyBought();
+        alreadybought.itemName = "";
+        alreadybought.itemQuantity = "";
+        alreadybought.addItem = function () {
+            ShoppingListCheckOffService.addItemAlreadyBought(alreadybought.itemName, alreadybought.itemQuantity);
+        };
+
+        alreadybought.removeItem = function (itemIdex) {
+            ShoppingListCheckOffService.removeItemAlreadyBought(itemIndex);
+        };
+    }
+
+
+    function ShoppingListCheckOffService() {
+        var service = this;
+
+        // List of items to buy
+        var itemsToBuy = shoppingList;
+
+        // List of items already bought
+        var itemsAlreadyBought = []
+
+        service.addItemToBuy = function (itemName, quantity) {
+            var item = {
+                name: itemName,
+                quantity: quantity
+            };
+            itemsToBuy.push(item);
+        };
+
+        service.addItemAlreadyBought = function (itemName, quantity) {
+            var item = {
+                name: itemName,
+                quantity: quantity
+            };
+            itemsAlreadyBought.push(item);
+        };
+
+        service.removeItemToBuy = function (itemIdex) {
+            itemsToBuy.splice(itemIdex, 1);
+        };
+
+        service.removeItemAlreadyBought = function (itemIdex) {
+            itemsAlreadyBought.splice(itemIdex, 1);
+        };
+
+        service.getItemsToBuy = function () {
+            return itemsToBuy;
+        };
+
+        service.getItemsAlreadyBought = function () {
+            return itemsAlreadyBought;
+        }
+
+    }
+
 })();
-
-
-//var str = 'The quick brown fox jumped over the lazy dog.';
-
-//var words = str.split(',');
-
-//console.log(words[3]);
-// expected output: "fox"
-
-//var chars = str.split('');
-//console.log(chars[8]);
-// expected output: "k"
-
-//var strCopy = str.split();
-//console.log(strCopy);
-// expected output: Array ["The quick brown fox jumped over the lazy dog."]
